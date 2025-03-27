922d0ddce8c49ac1d5890bf911d486af72052df2
parth14899 <parth14899james@gmail.com>
2025-03-27 15:35:31 -0400

    Created Exercise  class with attributes: ExerciseId, Name, Description, Duration, Intensity, MuscleGroup. Added constructor to initialize Exercise class with values.

---
+++ Exercise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+// Exercise.cs
+public class Exercise
+{
+    public string ExerciseId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+    public string Intensity { get; set; }
+    public string MuscleGroup { get; set; }
+
+    // Constructor
+    public Exercise(string exerciseId, string name, string description, int duration, string intensity, string muscleGroup)
+    {
+        ExerciseId = exerciseId;
+        Name = name;
+        Description = description;
+        Duration = duration;
+        Intensity = intensity;
+        MuscleGroup = muscleGroup;
+    }
+}
