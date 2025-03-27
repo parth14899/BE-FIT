9aca86cd726162ec95047d34f8fd9ef2c0135736
parth14899 <parth14899james@gmail.com>
2025-03-27 15:29:40 -0400

    Created User  class with attributes: UserId, FirstName, LastName, Email, Age, FitnessLevel. Added constructor to initialize User class with values.

---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class User
+{
+    public string UserId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public int Age { get; set; }
+
+    // fitness level  (beginner, intermediate, advanced)
+    public string FitnessLevel { get; set; }
+
+    // Constructor to initialize the User object
+    public User(string userId, string firstName, string lastName, string email, int age, string fitnessLevel)
+    {
+        UserId = userId;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Age = age;
+        FitnessLevel = fitnessLevel;
+    }
+}
+
